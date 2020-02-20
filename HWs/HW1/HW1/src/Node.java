d5530d0cf8686d7cc2471c11d26b27e26fdcc182
mehmetselimozcan <43796886+mehmetselimozcan@users.noreply.github.com>
2020-02-20 20:13:43 +0300

    Create HW1 folder and push initial code

---
+++ HWs/HW1/HW1/src/Node.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Node {
+
+    private int cannibalCount;
+    private int missionaryCount;
+
+    private List<Node> children = new ArrayList<>();
+
+    private Node parent = null;
+
+    public Node(int cannibalCount, int missionaryCount) {
+        this.cannibalCount = cannibalCount;
+        this.missionaryCount = missionaryCount;
+    }
+
+    public Node addChild(Node child) {
+        child.setParent(this);
+        this.children.add(child);
+        return child;
+    }
+
+    public void addChildren(List<Node> children) {
+        children.forEach(each -> each.setParent(this));
+        this.children.addAll(children);
+    }
+
+    public List<Node> getChildren() {
+        return children;
+    }
+
+    public int getCannibalCount() {
+        return cannibalCount;
+    }
+
+    public int getMissionaryCount() {
+        return missionaryCount;
+    }
+
+    private void setParent(Node parent) {
+        this.parent = parent;
+    }
+
+    public Node getParent() {
+        return parent;
+    }
+
+}
---
+++ HWs/HW1/HW1/src/search.java
@@ -0,0 +1,89 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class search {
+    public static void main(String[] args){
+        Node root = new Node(4,4);
+        boolean west = true;
+
+        List<int[]> previousStatesWest = new ArrayList<>();
+        List<int[]> previousStatesEast = new ArrayList<>();
+
+        int stateWest[] = {4,4};
+        int stateEast[] = {0,0};
+
+        previousStatesWest.add(stateWest);
+        previousStatesEast.add(stateEast);
+
+        root = createTree(root, west, previousStatesWest, previousStatesEast);
+
+        List<Node> list = root.getChildren();
+        for(int j = 0; j< list.size(); j++){
+            System.out.println(list.get(j).getCannibalCount() + " " + list.get(j).getMissionaryCount());
+        }
+    }
+
+    public static Node createTree(Node n, boolean west, List<int[]> previousStatesWest, List<int[]> previousStatesEast){
+        if(n == null){
+            return null;
+        }
+
+        List<int[]> previousStates = new ArrayList<>();
+        previousStates = previousStatesWest;
+
+        int sign = -1;
+
+        List<int[]> possibilities = new ArrayList<>();
+        int [][] cases = {{2,0},{0,2},{1,1},{1,0},{0,1}};
+
+        if(west){
+            for(int index = 0; index < cases.length; index++){
+                if(n.getCannibalCount() >= cases[index][0] && n.getMissionaryCount() >= cases[index][1]){
+                    possibilities.add(cases[index]);
+                }
+            }
+        }
+
+        if(!west){
+            previousStates = previousStatesEast;
+            sign = 1;
+            for(int index = 0; index < cases.length; index++){
+                if(4-n.getCannibalCount() >= cases[index][0] && 4-n.getMissionaryCount() >= cases[index][1]){
+                    possibilities.add(cases[index]);
+                }
+            }
+        }
+
+
+        for(int index = 0; index < possibilities.size(); index++){
+            boolean loop = false;
+
+            int newCannibalCount = n.getCannibalCount() + sign*(possibilities.get(index)[0]);
+            int newMissionaryCount = n.getMissionaryCount() + sign*(possibilities.get(index)[1]);
+
+            for(int j = 0; j< previousStates.size(); j++){
+                if((newCannibalCount == previousStates.get(j)[0]) && (newMissionaryCount == previousStates.get(j)[1])){
+                    loop = true;
+                }
+            }
+            if(!loop && ((newCannibalCount <= newMissionaryCount) || newMissionaryCount == 0) && (newMissionaryCount == 4 || (4-newCannibalCount <= 4-newMissionaryCount))){
+                Node child = new Node(newCannibalCount , newMissionaryCount);
+                int currentState[] = {newCannibalCount , newMissionaryCount};
+                previousStates.add(currentState);
+                n.addChild(child);
+                if(!west){
+                    previousStatesEast = previousStates;
+                } else {
+                    previousStatesEast = previousStates;
+                }
+                createTree(child,!west,previousStatesWest, previousStatesEast);
+            }
+        }
+
+
+
+        return n;
+    }
+}
